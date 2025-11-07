d86b9dc4c18e86c39268711616483366d9483277
Kusum gajare <kusumgajare2004@gmail.com>
2025-11-07 16:05:20 +0530

    Initial Commit

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-export default[
+const travelData=[
     {
         id:1,
-        title: "Mount Fuji",
-        location: "JAPAN",
-        googleMapsUrl: "https://maps.app.goo.gl/My6JP23Hwtna1thJ9",
+        title:"Mount Fuji",
+        location:"JAPAN",
+        googleMapsUrl:"https://maps.app.goo.gl/My6JP23Hwtna1thJ9",
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",    
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
@@ -31,3 +31,6 @@ export default[
     }
     
 ];
+
+
+export default travelData;
